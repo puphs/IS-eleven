df785fd47873edbace2b002d1b4cf69d25f7f390
puphs <43395605+puphs@users.noreply.github.com>
2020-10-26 09:21:29 +0400

    Graph improvements

--- js/Graph.js
+++ js/Graph.js
@@ -18,28 +18,32 @@ function Graph(options) {
     let ctx = canvas.getContext('2d');
 
     function xs(x) {
-        return x - WINDOW.LEFT;
-        //return (x - WINDOW.LEFT) / WINDOW.width * canvas.width; 
+        return (x - WINDOW.LEFT) / WINDOW.WIDTH * canvas.width; 
     }
 
     function ys(y) {
-        return y - WINDOW.BOTTOM;
-        //return canvas.height - (y - WINDOW.BOTTOM) / WINDOW.height * canvas.height; 
+        return canvas.height - (y - WINDOW.BOTTOM) / WINDOW.HEIGHT * canvas.height; 
     }
 
 
     this.clear = function() {
-        ctx.fillStyle = '#dddddd';
+        ctx.fillStyle = '#ffffff';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
     }
 
     this.line = function(x1, y1, x2, y2, color = 'red', width = 2) {
         ctx.beginPath();
         ctx.strokeStyle = color;
-        ctx.width = width;
+        ctx.lineWidth = width;
         ctx.moveTo(xs(x1), ys(y1));
         ctx.lineTo(xs(x2), ys(y2));
         ctx.stroke();
+    }
+
+    this.text = function(x, y, text, color = 'black', fontSize = 8) {
+        ctx.font = fontSize + 'px Arial';
+        ctx.fillStyle = color;
+        ctx.fillText(text, xs(x), ys(y));
     }
 
 }
--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 window.onload = function() {
     const WINDOW = {
-        LEFT: -150,
-        BOTTOM: -150,
-        WIDTH: 150,
-        HEIGHT: 150,
+        LEFT: -10,
+        BOTTOM: -10,
+        WIDTH: 20,
+        HEIGHT: 20,
     }
     
     let graph = new Graph({
@@ -15,17 +15,49 @@ window.onload = function() {
     
     function drawFunction() {
         let x = WINDOW.LEFT;
-        var dx = WINDOW.WIDTH / 100;
+        var dx = WINDOW.WIDTH / 1000;
     
         while (x <  WINDOW.LEFT + WINDOW.WIDTH) {
-            graph.line(x, f(x), x + dx, f(x + dx));
-            console.log(x);
+            graph.line(x, f(x), x + dx, f(x + dx), "red", 5);
             x += dx;
         }
+    }
+
+    function drawOXY() {
+        graph.line(WINDOW.LEFT, 0, WINDOW.WIDTH + WINDOW.LEFT, 0, 'black')
+        graph.line(0, WINDOW.BOTTOM, 0, WINDOW.BOTTOM + WINDOW.HEIGHT, 'black')
+    }
+
+    function drawGrid(color = "#999999", lineSize = 1) {
+        for (let x = WINDOW.LEFT; x < WINDOW.WIDTH; x++) {
+            graph.line(x, WINDOW.BOTTOM, x, WINDOW.BOTTOM + WINDOW.HEIGHT, color, lineSize);
+        }
+        for (let y = WINDOW.LEFT; y < WINDOW.WIDTH; y++) {
+            graph.line(WINDOW.LEFT, y, WINDOW.LEFT + WINDOW.WIDTH, y, color, lineSize);
+        }
+    }
+
+    function drawSegments(color = 'black', unitSize = 0.2, fontSize = 10) {
+        for (let x = WINDOW.LEFT; x < WINDOW.WIDTH; x++) {
+            if (x == 0)
+                continue;
+            graph.line(x, -unitSize / 2, x, unitSize / 2, color)
+            graph.text(x, unitSize, x, 'black', fontSize);
+        }
+        for (let y = WINDOW.LEFT; y < WINDOW.WIDTH; y++) {
+            if (y == 0)
+                continue;
+            graph.line(-unitSize / 2, y, unitSize / 2, y, color);
+            graph.text(unitSize, y, y, 'black', fontSize);
+        }
+        graph.text(unitSize, -unitSize * 2, 0, 'black', fontSize + 'px');
     }
     
     function render() {
         graph.clear();
+        drawGrid();
+        drawOXY();
+        drawSegments();
         drawFunction();
     }
 
